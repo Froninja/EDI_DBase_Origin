7e19ac7adc895b81c9351d48574dba6102dc27b4
Jacob Froman <jacob.h.froman@gmail.com>
2015-09-12 16:03:03 -0700

    Alpha 2

--- MB_EDI_DB.py
+++ MB_EDI_DB.py
@@ -1,10 +1,11 @@
 from PODatabase import PODatabase
 from PurchaseOrder import PurchaseOrder
 
-print('Hello world')
-
 db = PODatabase()
 
 db.read_db("C:\Users\Jacob\Documents\PO_DB.csv")
-
+for customer in db.export_paths.iterkeys():
+    db.read_export(customer)
 db.console_print()
+db.write_db("C:\Users\Jacob\Documents\PO_DB.csv")
+input = raw_input("")
--- PODatabase.py
+++ PODatabase.py
@@ -4,8 +4,11 @@ class PODatabase(object):
     """Stores a set of PurchaseOrder objects and represents them to the user"""
     def __init__(self):
         self.purchase_orders = dict()
-        #for purchase_order in purchase_orders:
-            #self.purchase_orders[purchase_order.po_number] = purchase_order
+        self.export_paths = dict()
+        with open("Config.txt") as config:
+            for line in config:
+                line = line.split(': ')
+                self.export_paths[line[0]] = line[1].rstrip('\n').rstrip('\r')
 
     def console_print(self):
         """Prints the information on all POs in the database to the console"""
@@ -23,26 +26,70 @@ class PODatabase(object):
 
     def write_db(self, db_file):
         """Writes all PO information to the supplied file"""
-        with open(db_file, 'w') as db:
-            write_string = ""
-            for PO in self.purchase_orders.itervalues():
-                write_string += "{},{},{},{},{},{},{},{},{}".format(
-                    PO.po_number, PO.customer, PO.creation_date, PO.start_ship,
-                    PO.cancel_ship, PO.total_cost, PO.discount, PO.label,
-                    PO.complete)
-            db.write(write_string)
+        if len(self.purchase_orders) > 0:
+            with open(db_file, 'w') as db:
+                write_string = ""
+                for PO in self.purchase_orders.itervalues():
+                    write_string += "{},{},{},{},{},{},{},{},{}\n".format(
+                        PO.po_number, PO.customer, PO.creation_date, PO.start_ship,
+                        PO.cancel_ship, PO.total_cost, PO.discount, PO.label,
+                        PO.complete)
+                db.write(write_string)
 
     def read_db(self, db_file):
         """Reads POs from supplied CSV file and adds to the database object"""
         with open(db_file, 'r') as db:
             for line in db:
-                line = line.replace('\n','').replace('\r','').split(',')
-                PO = PurchaseOrder(line[0], line[1])
-                PO.creation_date = line[2]
-                PO.start_ship = line[3]
-                PO.cancel_ship = line[4]
-                PO.total_cost = line[5]
-                PO.discount = line[6]
-                PO.label = line[7]
-                PO.complete = line[8]
-                self.purchase_orders[PO.po_number] = PO
+                if len(line) > 1:
+                    line = line.replace('\n','').replace('\r','').split(',')
+                    PO = PurchaseOrder(line[0], line[1])
+                    PO.creation_date = line[2]
+                    PO.start_ship = line[3]
+                    PO.cancel_ship = line[4]
+                    PO.total_cost = line[5]
+                    PO.discount = line[6]
+                    PO.label = line[7]
+                    PO.complete = line[8]
+                    self.purchase_orders[PO.po_number] = PO
+
+    def read_export(self, customer):
+        """Reads the TLW export based on customer name. If the PO number is not
+        already in the program data, adds it"""
+        export_file = self.get_export_file(customer)
+        with open(export_file, 'r') as export:
+            for line in export:
+                line = line.rstrip('\n').rstrip('\r').split(',')
+                if not self.check_export_against_db(line[1]):
+                    PO = PurchaseOrder(line[1], customer)
+                    PO.cancel_ship = line[4]
+                    PO.total_cost = self.get_cost_from_exp(PO.po_number,
+                                                           export_file)
+                    self.purchase_orders[PO.po_number] = PO
+
+    def get_cost_from_exp(self, po_num, export_file):
+        """Searches the TLW export for all lines matching the supplied PO
+        number and returns total cost based on the sum of each line's cost"""
+        total_cost = 0
+        with open(export_file, 'r') as export:
+            for line in export:
+                #if len(line) > 1:
+                line = line.rstrip('\n').rstrip('\r').split(',')
+                print line
+                if line[1] == po_num:
+                    total_cost += float(line[7]) * int(line[9])
+        return total_cost
+
+    def check_export_against_db(self, PO_num):
+        """For a given PO Number in the export file, checks against the data
+        file loaded at program start. If the PO already exists, returns false
+        """
+        if PO_num != "PO #":
+            if PO_num in self.purchase_orders:
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    def get_export_file(self, customer):
+        return self.export_paths[customer]
